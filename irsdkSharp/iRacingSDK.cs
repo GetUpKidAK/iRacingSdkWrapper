41416b634abc13a7ebb3793ee6a61d82f3bcb6b5
Ash <ash.kendall@gmail.com>
2021-06-13 00:38:50 +0100

    Added VideoCapture value to BroadcastMessageTypes

--- irsdkSharp/iRacingSDK.cs
+++ irsdkSharp/iRacingSDK.cs
@@ -8,7 +8,7 @@ using System.IO.MemoryMappedFiles;
 
 namespace iRSDKSharp
 {
-    public enum BroadcastMessageTypes { CamSwitchPos = 0, CamSwitchNum, CamSetState, ReplaySetPlaySpeed, ReplaySetPlayPosition, ReplaySearch, ReplaySetState, ReloadTextures, ChatCommand, PitCommand, TelemCommand };
+    public enum BroadcastMessageTypes { CamSwitchPos = 0, CamSwitchNum, CamSetState, ReplaySetPlaySpeed, ReplaySetPlayPosition, ReplaySearch, ReplaySetState, ReloadTextures, ChatCommand, PitCommand, TelemCommand, VideoCapture = 13 };
     public enum CamSwitchModeTypes { FocusAtIncident = -3, FocusAtLeader = -2, FocusAtExciting = -1, FocusAtDriver = 0 };
     public enum CameraStateTypes { None = 0x0000, IsSessionScreen = 0x0001, IsScenicActive = 0x0002, CamToolActive = 0x0004, UIHidden = 0x0008, UseAutoShotSelection = 0x0010, UseTemporaryEdits = 0x0020, UseKeyAcceleration = 0x0040, UseKey10xAcceleration = 0x0080, UseMouseAimMode = 0x0100 };
     public enum ReplayPositionModeTypes { Begin = 0, Current, End };
