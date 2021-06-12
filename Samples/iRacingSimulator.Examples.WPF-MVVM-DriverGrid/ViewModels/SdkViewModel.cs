9cd47eba249ec15fe27ca39722cc1a44ec516900
Ash <ash.kendall@gmail.com>
2021-06-13 00:33:47 +0100

    Added solution, updated gitignore

--- Samples/iRacingSimulator.Examples.WPF-MVVM-DriverGrid/ViewModels/SdkViewModel.cs
+++ Samples/iRacingSimulator.Examples.WPF-MVVM-DriverGrid/ViewModels/SdkViewModel.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
-using iRacingSimulator.Examples.WPF_MVVM_DriverGrid.Annotations;
+
 
 namespace iRacingSimulator.Examples.WPF_MVVM_DriverGrid.ViewModels
 {
